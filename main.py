3773e73fb55003b75e06432face93fc41f1f9db7
jaedeok-lee1023 <jaedeok.lee_b@kurlycorp.com>
2026-02-10 19:18:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
             f">:ck11: 스탭(정규직) 이상 : (직접 추천) *추천 대상자 근속 3개월 후 500,000원 / 추천대상자 근속 6개월 후 500,000원*\n\n"
             f"\n"
             f":purple_heart: *채용 및 추천 관련 문의* :purple_heart:\n"
-            f"*:slack: 문의사항 : <@U094ZMCF1T2>,<@U05P7LCBQ8H>\n"
+            f"*:slack: 문의사항 : <@U094ZMCF1T2>,<@U05P7LCBQ8H>*\n"
             f":phone:  *010-5820-6937*\n\n"
             f"\n"
             f"감사합니다.\n"
